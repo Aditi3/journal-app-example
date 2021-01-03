c904063136dccb16c0c86b5557e55a1cb79d8e18
Aditi3 <aditi.gvc@gmail.com>
2021-01-03 08:16:37 +0530

    Add General Flow of the Application

---
+++ HelloJournal/HelloJournal/HJCreateJournalViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HJCreateJournalViewController.swift
+//  HelloJournal
+//
+//  Created by Aditi Agrawal on 03/01/21.
+//  Copyright © 2021 Aditi Agrawal. All rights reserved.
+//
+
+import UIKit
+
+class HJCreateJournalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ HelloJournal/HelloJournal/HJJournalDetailVC.swift
@@ -0,0 +1,30 @@
+//
+//  HJDetailVC.swift
+//  HelloJournal
+//
+//  Created by Aditi Agrawal on 02/01/21.
+//  Copyright © 2021 Aditi Agrawal. All rights reserved.
+//
+
+import UIKit
+
+class HJJournalDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
